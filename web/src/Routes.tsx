db29863578467f2bb5db3c0a798e66d2d744f97b
ecampos <ecampos.jr54@gmail.com>
2024-09-04 13:51:16 -0500

    generate about page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -14,6 +14,7 @@ import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
       <Set wrap={ScaffoldLayout} title="Events" titleTo="events" buttonLabel="New Event" buttonTo="newEvent">
         <Route path="/events/new" page={EventNewEventPage} name="newEvent" />
